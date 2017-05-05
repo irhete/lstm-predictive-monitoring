07da7c80b24266993c3838f13bed7c3a55d0c266
Irene Teinemaa <hpc_irheta@juur.grid.eenet.ee>
2017-05-05 12:04:36 +0300

    Basic with 15 epochs

--- discriminative_lstm/run.sh
+++ discriminative_lstm/run.sh
@@ -2,10 +2,10 @@
 
 #SBATCH --partition=gpu
 #SBATCH --gres=gpu:1
-#SBATCH --output=/storage/anna_irene/lstm_output_softsign.txt
+#SBATCH --output=/storage/anna_irene/lstm_output.txt
 #SBATCH --mem=20000
 
 THEANO_FLAGS=mode=FAST_RUN,device=gpu0,floatX=float32
-python run_LSTM_softsign.py
+python run_LSTM.py
 
 chmod -R 777 /storage/anna_irene
